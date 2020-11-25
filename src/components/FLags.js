6259ff5b6004c57b7c423fb7fc56cfe5a1ece142
anderson <anderson.d.cardoso92@gmail.com>
2020-11-25 20:08:31 +0400

    first commit

---
+++ src/components/FLags.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect, Fragment} from 'react';
+
+const Flags = () => {
+
+    return (
+        <>
+            <div id="flags-display">
+
+            </div>
+        </>
+    );
+}
+
+export default Flags;
